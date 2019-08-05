bae69fb417e40f11b94030ccaaa749a2ad0fc642
emilyq6 <52425823+emilyq6@users.noreply.github.com>
2019-08-04 21:10:53 -0400

    updated javascript in index

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
- alert('Quote Of The Day: When you focus on the good, the good gets better :)');
+let date = new Date();
+let day = date.getDay();
+
+if (day=0){
+ alert("Quote Of The Day: Keep looking up, that's the secret to life :)");
+}
+
+else if (day=1) {
+  alert("Quote Of The Day: Life is 10% what happens to you and 90% how you react to it")
+}
+
+else if (day=2) {
+  alert("Quote Of The Day: Push yourself, because no one else is going to do it for you.")
+}
+
+else if (day=3) {
+  alert("Quote Of The Day: Life is like riding a bicycle, to keep your balance you must keep moving")
+}
+
+else if (day=4) {
+  alert("Quote Of The Day:Beautiful things happen when you distance yourself from negativity")
+}
+
+else if (day=5) {
+  alert("Quote Of The Day: When you can't find the sunshine, be the sunshine")
+}
+
+else if (day=6) {
+  alert("Quote Of The Day: When you focus on the good, the good gets better :)")
+}
+
 
 function java(){
   window.alert("have a great day!")
